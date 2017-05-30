c7cf1a997e4d0f3a9eeeb0c233ade3cf6fb1d765
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-05-30 13:00:28 +0100

    Created init.sh install google chrome code added.

---
+++ init.sh
@@ -0,0 +1,13 @@
+# Install Google Chrome
+sudo apt-get update
+sudo apt-get -f install
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+sudo apt autoremove
+rm -rf google-chrome*
+
+# Setup git
+git config --global user.email "james.d.mcgarr@students.ittralee.ie"
+git config --global user.name "JamesDaniel"
+
