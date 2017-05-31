43d5c9616b0d424a5aca5bb3b97bac7ce3a7b3bf
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-05-31 06:32:04 +0100

    init.sh fix

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 # Install Google Chrome
 sudo apt-get update
-sudo apt-get -f install
 sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo apt-get -f install
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo apt autoremove
