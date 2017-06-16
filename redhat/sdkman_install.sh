5c0c4103e2060b221ff7dcd8604100d28630ffd4
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-06-16 22:55:42 +0100

    added sdkman_install.sh

---
+++ redhat/sdkman_install.sh
@@ -0,0 +1,9 @@
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+
+# Install Gradle
+sdk install gradle
+sdk install java
+sdk install maven
+sdk install springboot
