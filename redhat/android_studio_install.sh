1bce83fc62997b19bec6193d1042349d61cd8d2b
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-06-17 00:44:42 +0100

    android studio install

---
+++ redhat/android_studio_install.sh
@@ -0,0 +1,6 @@
+# Install android studio.
+
+wget https://dl.google.com/dl/android/studio/ide-zips/2.3.3.0/android-studio-ide-162.4069837-linux.zip
+unzip android-studio-ide-162.4069837-linux.zip
+sudo mv android-studio/ /opt/android-studio/
+/usr/share/android-studio/bin/studio.sh
