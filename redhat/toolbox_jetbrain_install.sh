c9edc3ec4c20628e40cdba2b1ddcc6ef0763e06e
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-06-16 23:14:05 +0100

    jetbrains toolbox install

---
+++ redhat/toolbox_jetbrain_install.sh
@@ -0,0 +1,8 @@
+# Install JetBrains Toolbox
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.3.2421.tar.gz
+tar xzfv jetbrains-toolbox-1.3.2421.tar.gz
+sudo mv jetbrains-toolbox-1.3.2421 /usr/share/jetbrains-toolbox-1.3.2421
+rm jetbrains-toolbox-1.3.2421.tar.gz
+echo 'To run Toolbox Execute the following commands as a user:'
+echo 'cd /opt/jetbrains-toolbox-1.3.2421/'
+echo './jetbrains-toolbox'
