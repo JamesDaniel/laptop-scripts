e8f0a4e85b5ce96fba8f8f45b395d705ddef8051
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-06-17 00:16:32 +0100

    activemq and chrome install

---
+++ redhat/chrome_install.sh
@@ -0,0 +1,6 @@
+# Install Google Chrome web browser.
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf install lsb
+sudo rpm I google-chrome-stable_current_x86_64.rpm
+rm google-chrome-stable_current_x86_64.rpm
