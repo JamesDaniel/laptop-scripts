2c30b4cbd6e2e8fbe55d8f93ad69df09a33d56cb
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-07-02 18:02:11 +0100

    removed redundant google chrome installation script in init.sh

--- debain/init.sh
+++ debain/init.sh
@@ -1,11 +1,4 @@
-# Install Google Chrome
 sudo apt-get update
-sudo apt-get install libxss1 libappindicator1 libindicator7
-sudo apt-get -f install
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-sudo apt autoremove
-rm -rf google-chrome*
 
 # Setup git
 git config --global user.email "james.d.mcgarr@students.ittralee.ie"
